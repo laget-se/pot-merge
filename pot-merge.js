058a2d7f1d7c75dce62168eca27dcd76ecd32b2e
Daniel Storm <daniel.storm@laget.se>
2017-09-22 14:14:11 +0200

    Disregard whitespaces when building the hash for the blocks

--- pot-merge.js
+++ pot-merge.js
@@ -64,7 +64,7 @@ Block.prototype.hash = function() {
             }
 
             if(record) {
-                msgid += arr[i];
+                msgid += arr[i].replace(/\"/g, '');
             }
         }
         return msgid;
